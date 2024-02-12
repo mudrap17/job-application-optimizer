85ce5bcebe05a3ad5525548ec6434a956b42e4fb
Mudra Patel <mudra1710@outlook.com>
2024-02-12 11:33:26 -0600

    New features

--- resume_optimizer.py
+++ resume_optimizer.py
@@ -2,41 +2,67 @@ import os
 import google.generativeai as genai
 
 
-def llm_response(prompt_template,key):
-    genai.configure(api_key = key)
-    model = genai.GenerativeModel('gemini-pro')
+def llm_response(prompt_template, key):
+    genai.configure(api_key=key)
+    model = genai.GenerativeModel("gemini-pro")
     response = model.generate_content(prompt_template)
     return response.text
 
-def llm_result(job_description,resume,prompt,key):
-    prompt_template = f"""You are applying to a job. For given resume and job description,
-    {prompt}. Explain which keywords you added and what changes you made and why. \n resume =
-    {resume} , \n job description = {job_description}. Return a well formatted resume.""" 
-    return llm_response(prompt_template,key)
 
-def linkedin_jobs(resume,key):
+def llm_result(job_description, resume, prompt, key):
+    prompt_template = f"""You are applying to a job. For given resume and job description,
+    {prompt}. List the keywords you added and what other changes you made and why. \n resume =
+    {resume} , \n job description = {job_description}. Return a well formatted and organised resume with proper segmentation."""
+    return llm_response(prompt_template, key)
+
+
+def linkedin_jobs(resume, key):
     prompt_template = f"""Based on the experience from my resume, which job roles should I apply to on LinkedIn? 
     \n resume = {resume}
     Give a list of top 5 job roles like [Software Engineer,Mechanical Engineer] 
     and locations to apply with evidence supporting the results. 
     Give the LinkedIn job urls for the same."""
-    return llm_response(prompt_template,key)
-
-def cold_outreach(purpose, person, key):
-    prompt_template = f"""I'm reaching out to a {person}. Write a cold outreach message for this purpose: {purpose} """
-    return llm_response(prompt_template , key)
-
-def suggestions(text, person, key):
-    prompt_template = """I'm reaching out to a {person}. Here's my message to them:{text} .
-    Suggest me ways I can improve my writing skills and make the cold outreach message more refined.
-    Explain with example. If you were that person, would you engage back? """ 
     return llm_response(prompt_template, key)
 
 
+def cold_outreach(purpose, person, key):
+    prompt_template = f"""I'm reaching out to a {person}. Write a cold outreach message for this purpose: {purpose} """
+    return llm_response(prompt_template, key)
 
 
+def suggestions(text, person, key):
+    prompt_template = f"""I'm reaching out to a {person}. Here's my message to them:{text} .
+    Suggest me ways I can improve my writing skills and make the cold outreach message more refined.
+    Explain with example. If you were that person, would you engage back? """
+    return llm_response(prompt_template, key)
 
 
+def sample_resume(job_description, key):
+    prompt_template = f"""For the given job description: {job_description}, 
+    write a sample resume for an ideal candidate for this position. 
+    Include quantitative impact examples of experience and projects with most important keywords."""
+    return llm_response(prompt_template, key)
 
 
+def project_suggestions_job(job_description, key):
+    prompt_template = f"""For the given job description: {job_description}, 
+    give practical project suggestions to showcase my abilities to perform well in the job."""
+    return llm_response(prompt_template, key)
 
+
+def project_suggestions_skill(skills, domain, key):
+    prompt_template = f"""For the given job skills: {skills}, 
+    give practical project suggestions to showcase my abilities to use those skills in the domain : {domain} , if given."""
+    return llm_response(prompt_template, key)
+
+
+def roadmap(skill, key):
+    prompt_template = f"""You are an instructor. For the given skill: {skill}, 
+    design a roadmap to learn the skills with links to resources and practical examples."""
+    return llm_response(prompt_template, key)
+
+
+def interview(job_desc, key):
+    prompt_template = f"""For the given job description: {job_desc}, 
+    help me prepare for the interview by asking relevant questions as an employer."""
+    return llm_response(prompt_template, key)
--- streamlit_app.py
+++ streamlit_app.py
@@ -3,110 +3,242 @@ import numpy as np
 import streamlit as st
 from streamlit_option_menu import option_menu
 from PyPDF2 import PdfReader
-from resume_optimizer import llm_result,linkedin_jobs,cold_outreach,suggestions
+from resume_optimizer import (
+    llm_result,
+    linkedin_jobs,
+    cold_outreach,
+    suggestions,
+    sample_resume,
+    project_suggestions_job,
+    project_suggestions_skill,
+    roadmap,
+    interview,
+)
 
 # Sidebar section
 with st.sidebar:
-  st.subheader('Choose your task:')
-  option = option_menu(menu_title='', options=['Resume Optimizer', 'Cover letter', 'Job Industry Analysis', 'LinkedIn Jobs', 'Cold outreach Assist'],
-                         icons=['house-fill', 'file-text-fill', 'bar-chart-fill', 'linkedin','chat-right-text-fill'])
+    st.subheader("Enter the information to proceed")
+    key = st.text_input(label="Your Gemini API key", type="password")
+    st.subheader("Choose your task:")
+    option = option_menu(
+        menu_title="",
+        options=[
+            "Resume Optimizer",
+            "Sample resume",
+            "Cover letter",
+            "Job Industry Analysis",
+            "LinkedIn Jobs",
+            "Cold outreach Assist",
+            "Project Suggestions",
+            "Roadmap to skills",
+            "Interview prep",
+        ],
+        icons=[
+            "house-fill",
+            "file-text-fill",
+            "pencil-square",
+            "bar-chart-fill",
+            "linkedin",
+            "chat-right-text-fill",
+            "rocket",
+            "map-fill",
+            "person-check",
+        ],
+    )
 
-if option == 'Resume Optimizer' or option == 'Cover letter':
-    if option == 'Resume Optimizer':
-        prompt = "optimize the resume to include keywords from the job description, add quantitative results to support the relevant experience and show yourself as the perfect fit for the job."
-        st.title('AI powered Resume optimization')
-        st.write('Use this tool to optimize your resume according to the job description')
+if option == "Resume Optimizer" or option == "Cover letter":
+    if option == "Resume Optimizer":
+        prompt = """optimize the resume to include keywords from the job description,Using practical language, 
+        write three to five resume achievements based on my work experience. 
+        Add details about quantitative results and impact instead of listing responsibilities to support the relevant experience
+        and evidence to show you the perfect fit for the job."""
+        st.title("AI powered Resume optimization")
+        st.write(
+            "Use this tool to optimize your resume according to the job description"
+        )
     else:
-        prompt = "write a cover letter  for the given job description and inlcude relevant experience from the resume and add details to make you the perfect fit for the job."
-        st.title('AI powered Cover Letter optimization')
-        st.write('Use this tool to optimize your cover letter according to the job description')
-    
-    key = st.text_input(label='Enter your Gemini API key:',type='password')
-    job_desc = st.text_input(label='Enter the job description:')
+        prompt = """Using practical language, write a conversational, persuasive cover letter 
+        to include relevant keywords from the job descriptions 
+        and highlight the appropriate experience from the resume with required details to make you the perfect fit for the job. 
+        The cover letter should highlight skills beyond those mentioned in the resume."""
+        st.title("AI powered Cover Letter optimization")
+        st.write(
+            "Use this tool to optimize your cover letter according to the job description"
+        )
+
+    job_desc = st.text_input(label="Enter the job description:")
 
     # file upload
-    pdf = st.file_uploader(label='Upload your resume:', type='pdf')
-    resume_optimize=''
-    cover_letter =''
-    if option == 'Resume Optimizer':
-        resume_optimize = st.button(label='Optimize my resume')
+    pdf = st.file_uploader(label="Upload your resume:", type="pdf")
+    resume_optimize = ""
+    cover_letter = ""
+    if option == "Resume Optimizer":
+        resume_optimize = st.button(label="Optimize my resume")
     else:
-        cover_letter = st.button(label='Write an amazing cover letter')
-
-
+        cover_letter = st.button(label="Write an amazing cover letter")
+    if key is None:
+        st.info("Enter your Gemini API key on your left please")
     try:
         if pdf is not None:
-           pdf_reader = PdfReader(pdf)
+            pdf_reader = PdfReader(pdf)
 
-           # extract text from each page separately
-           text = ""
-           for page in pdf_reader.pages:
+            # extract text from each page separately
+            text = ""
+            for page in pdf_reader.pages:
                 text += page.extract_text()
-           if resume_optimize is not None or cover_letter is not None:
-               result = llm_result(job_desc, text ,prompt, key)
-               st.write(result)
-               
+            if resume_optimize is not None or cover_letter is not None:
+                if key is None:
+                    st.info("Enter your Gemini API key on your left please")
+                else:
+                    with st.spinner("Wait for it..."):
+                        result = llm_result(job_desc, text, prompt, key)
+                    st.write(result)
 
     except Exception as e:
-       st.warning(e)
+        st.warning(e)
 
-if option == 'Job Industry Analysis':
-    st.title('Job Industry Analysis')
-    st.subheader('Explore the industry and prominent states according to 2023 H1B approvals')
-    with st.spinner('Wait for it...'):
-      df = pd.read_excel('EmployerInformation.xlsx',engine='openpyxl')
-    df = df[['Employer (Petitioner) Name','Industry (NAICS) Code','Initial Approval','Petitioner State']]
-    option = st.selectbox("Select an industry", df['Industry (NAICS) Code'].unique(),index=None,placeholder="Select your industry")
+if option == "Sample resume":
+    st.title("Give a sample resume that is ideal for the given job description")
+    job_desc = st.text_input(label="Enter the job description:")
+    if key is None:
+        st.info("Enter your Gemini API key on your left please")
+    else:
+        resume = sample_resume(job_desc, key)
+        st.write(resume)
+
+
+if option == "Job Industry Analysis":
+    st.title("Job Industry Analysis")
+    st.subheader(
+        "Explore the industry and prominent states according to 2023 H1B approvals"
+    )
+    with st.spinner("Wait for it..."):
+        df = pd.read_excel("EmployerInformation.xlsx", engine="openpyxl")
+    df = df[
+        [
+            "Employer (Petitioner) Name",
+            "Industry (NAICS) Code",
+            "Initial Approval",
+            "Petitioner State",
+        ]
+    ]
+    option = st.selectbox(
+        "Select an industry",
+        df["Industry (NAICS) Code"].unique(),
+        index=None,
+        placeholder="Select your industry",
+    )
+
     if option:
-        cf = df[(df['Industry (NAICS) Code'] == option) & (df['Initial Approval'] > 0)]
-        st.info('Hover over this table to download, search or expand. Find the top 5 states data below the table.', icon="ℹ️")
-        st.write(cf[['Employer (Petitioner) Name','Initial Approval','Petitioner State']])
-        df1 = cf['Petitioner State'].value_counts().rename_axis('States').reset_index(name='Approval counts')
-        st.write('Top 5 states by approvals in this Industry')
-        st.bar_chart(df1[:5], x='States' , y= 'Approval counts',color = '#00ff11')
+        cf = df[(df["Industry (NAICS) Code"] == option) & (df["Initial Approval"] > 0)]
+        st.info(
+            "Hover over this table to download, search or expand. Find the top 5 states data below the table.",
+            icon="ℹ️",
+        )
+        st.write(
+            cf[
+                [
+                    "Employer (Petitioner) Name",
+                    "Initial Approval",
+                    "Petitioner State",
+                ]
+            ]
+        )
+        df1 = (
+            cf["Petitioner State"]
+            .value_counts()
+            .rename_axis("States")
+            .reset_index(name="Approval counts")
+        )
+        st.write("Top 5 states by approvals in this Industry")
+        st.bar_chart(df1[:5], x="States", y="Approval counts", color="#00ff11")
 
-    
 
-if option == 'LinkedIn Jobs':
-    st.title('LinkedIn jobs based on your resume')
-    key = st.text_input(label='Enter your Gemini API key:',type='password')
-    # file upload
-    pdf = st.file_uploader(label='Upload your resume:', type='pdf')
-    search_jobs = st.button('Search for jobs')
+if option == "LinkedIn Jobs":
+    st.title("LinkedIn jobs based on your resume")
+    if key is None:
+        st.info("Enter your Gemini API key on your left please")
+    pdf = st.file_uploader(label="Upload your resume:", type="pdf")
+    search_jobs = st.button("Search for jobs")
 
     try:
         if pdf is not None and key is not None:
-           pdf_reader = PdfReader(pdf)
+            pdf_reader = PdfReader(pdf)
 
-           # extract text from each page separately
-           text = ""
-           for page in pdf_reader.pages:
+            # extract text from each page separately
+            text = ""
+            for page in pdf_reader.pages:
                 text += page.extract_text()
-           if search_jobs:
-               result = linkedin_jobs(text,key)
-               st.write(result)
-    
-    except Exception as e:
-       st.warning(e)
+            if search_jobs:
+                result = linkedin_jobs(text, key)
+                st.write(result)
 
-if option == 'Cold outreach Assist':
-    st.title('Cold Outreach Assist')
-    st.write('Use AI to help you write better cold outreach messages')
-    key = st.text_input(label='Enter your Gemini API key:',type='password')
-    person = st.text_input(label='Whom are you reaching out to?')
-    purpose = st.text_input(label= 'What would you like to talk about?')
-    tab1,tab2 = st.tabs(['Draft first message','Get suggestions'])
-    with tab1:
-        st.title('Write a cold outreach message')
-        cm =st.button('Sample cold message')
-        if cm:
-            result = cold_outreach(purpose, person ,key)
-            st.write(result)
-    
-    with tab2:
-        st.title('Give suggestions to improve')
-        text = st.text_input('What did you have in mind?')
-        sug =st.button('Rewrite mine')
-        if sug:
-            result = suggestions(text, person ,key)
-            st.write(result)
+    except Exception as e:
+        st.warning(e)
+
+if option == "Cold outreach Assist":
+    st.title("Cold Outreach Assist")
+    st.write("Use AI to help you write better cold outreach messages")
+    person = st.text_input(label="Whom are you reaching out to?")
+    purpose = st.text_input(label="What would you like to talk about?")
+    tab1, tab2 = st.tabs(["Draft first message", "Get suggestions"])
+    if key is None:
+        st.info("Enter your Gemini API key on your left please")
+    else:
+        with tab1:
+            st.title("Write a cold outreach message")
+            cm = st.button("Sample cold message")
+            if cm:
+                result = cold_outreach(purpose, person, key)
+                st.write(result)
+
+        with tab2:
+            st.title("Give suggestions to improve")
+            text = st.text_input("What did you have in mind?")
+            sug = st.button("Rewrite mine")
+            if sug:
+                result = suggestions(text, person, key)
+                st.write(result)
+
+if option == "Project Suggestions":
+    st.title("Project Suggestions")
+    st.write(
+        "Get suggestions for projects including the requested skills and/or domain"
+    )
+    tab1, tab2 = st.tabs(
+        ["Related to given job description", "According to skills/domain "]
+    )
+    if key is None:
+        st.info("Enter your Gemini API key on your left please")
+    else:
+        with tab1:
+            job_desc = st.text_input(label="Give job description")
+            project = project_suggestions_job(job_desc, key)
+            st.write(project)
+        with tab2:
+            skills = st.text_input(label="Give list of skills")
+            domain = st.text_input(label="Give the domain name(sustainability,finance)")
+            project = project_suggestions_skill(skills, domain, key)
+            st.write(project)
+
+if option == "Roadmap to skills":
+    st.title("Roadmap to a skill of your choice")
+    skill = st.text_input("Give the skill name:")
+    if key is None:
+        st.info("Enter your Gemini API key on your left please")
+    else:
+        roadmap_skill = roadmap(skill, key)
+        st.write(roadmap_skill)
+
+if option == "Networking events search":
+    st.title("Networking event")
+    st.write("Get a list of networking events for given domain and dates")
+
+if option == "Interview prep":
+    st.title("Interview prep")
+    job_desc = st.text_input("Give the job description you're preparing for:")
+    if key is None:
+        st.info("Enter your Gemini API key on your left please")
+    else:
+        questions = interview(job_desc, key)
+        st.write(questions)
